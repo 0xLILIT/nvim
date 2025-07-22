e2c3204042feb16745776b44e8e3cdccc6bf1b19
Freja <143435200+yuki9k@users.noreply.github.com>
2025-07-22 13:01:22 +0200

    Initial commit

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,6 +5,6 @@ return {
 		-- vim.lsp.config("*", {
 		-- 	capabilities = require("blink.cmp").get_lsp_capabilities(),
 		-- })
-		vim.lsp.enable({ "cssls", "html", "lua_ls", "ts_ls" })
+		vim.lsp.enable({ "jdtls", "cssls", "html", "lua_ls", "ts_ls" })
 	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,31 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" , "css", "typescript", "scss", "sql", "php", "json"},
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
- }
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"java",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"elixir",
+				"heex",
+				"javascript",
+				"html",
+				"css",
+				"typescript",
+				"scss",
+				"sql",
+				"php",
+				"json",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
